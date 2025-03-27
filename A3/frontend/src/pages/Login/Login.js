125202c83e583d0b287b7e1ccfa8016cb2d302fa
Anson Lau <ansonlau57@gmail.com>
2025-03-27 04:54:50 -0400

    Updated Login with AuthContext

--- A3/frontend/src/pages/Login/Login.js
+++ A3/frontend/src/pages/Login/Login.js
@@ -1,36 +1,26 @@
 //import LoginForm from "../components/Auth/LoginForm";
-import {useState} from "react";
+import {useState, useContext} from "react";
 import {Box, Button, Container, TextField, Typography, Alert} from "@mui/material";
-import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import AuthContext from "../../context/AuthContext";
 
 export default function Login() {
     const [ utorid, setUtorid ] = useState("");
     const [ password, setPassword ] = useState("");
     const [ error, setError ] = useState("");
     const navigate = useNavigate();
+    const { login } = useContext(AuthContext);
 
     const handleLogin = async (e) => {
       e.preventDefault();
         try{
-          const response = await axios.post("http://localhost:3000/auth/tokens", {
-            utorid,
-            password
-          });
-
-          const { token, tokenExpiry } = response.data;
-          localStorage.setItem("token", token);
-          localStorage.setItem("tokenExpiry", tokenExpiry);
+          await login({utorid, password});
 
           // not implemented yet
           //navigate("/dashboard");
-          alert("Login successful");
 
         }catch(e){
-          if (e.response && e.response.data && e.response.data.message) {
-            setError(e.response.data.message);
-            alert("Utorid or password is incorrect");
-          }
+          setError(e.message || "Invalid utorid or password");
         }
     };
 
@@ -58,6 +48,7 @@ export default function Login() {
                 required
                 fullWidth
               />
+              {error && <Alert severity="error">{error}</Alert>}
             <Button type="submit" variant="contained" color="primary">
               Login
             </Button>
