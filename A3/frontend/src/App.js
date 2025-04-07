2f9d66e2c80e27c04fa7aef4518b1ae96db3e2fe
Fiona Hoang <hhpfiona@gmail.com>
2025-04-06 21:13:44 -0400

    updated app.js

--- A3/frontend/src/App.js
+++ A3/frontend/src/App.js
@@ -1,32 +1,48 @@
-import logo from './assets/logo.svg';
 import './styles/App.css';
 import React from 'react'; 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; 
+
 import Login from './pages/Login/Login'; 
 import Dashboard from './pages/Dashboard'; 
 import Transactions from './pages/Transactions';
 import ProtectedRoute from './route/ProtectedRoute';
+import OrganizerEvents from './pages/Organizer/OrganizerEvents';
+import EventManage     from './pages/Organizer/EventManage';
 
-function App() { 
-    return ( 
-        <BrowserRouter>
-            <Routes> 
-                <Route path="/login" element={<Login />} /> 
-                <Route path="/dashboard" element={
-                    <ProtectedRoute>
-                        <Dashboard />
-                    </ProtectedRoute>
-                } /> 
-                <Route path="/transactions" element={
-                    <ProtectedRoute>
-                        <Transactions />
-                    </ProtectedRoute>
-                } />
-                <Route path="/" element={<Navigate to="/dashboard" replace />} />
-            </Routes>
-        </BrowserRouter> 
-    ); 
+function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          {/* public */}
+          <Route path="/login" element={<Login />} />
+  
+          {/* protected */}
+          <Route
+            path="/dashboard"
+            element={<ProtectedRoute><Dashboard /></ProtectedRoute>}
+          />
+          <Route
+            path="/transactions"
+            element={<ProtectedRoute><Transactions /></ProtectedRoute>}
+          />
+  
+          {/* organizer */}
+          <Route
+            path="/organizer/events"
+            element={<ProtectedRoute><OrganizerEvents /></ProtectedRoute>}
+          />
+          <Route
+            path="/organizer/events/:id"
+            element={<ProtectedRoute><EventManage /></ProtectedRoute>}
+          />
+  
+          {/* default redirect */}
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        </Routes>
+      </BrowserRouter>
+    );
 }
+  
 
 // setup uses react-router-dom to change the browser URL 
 // when navigating between pages and allows back/forward navigation.
